13a5becc63a72ea5333592808bc48bb297a60636
Pierre Enriquez <pierre@perxtech.com>
2019-05-19 09:57:20 +0800

    Remove storyboard.
    Layout will be created programatically.

--- RefactorMe/RefactorMe/ViewController.swift
+++ RefactorMe/RefactorMe/ViewController.swift
@@ -12,36 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
         
         let tf = UITextView(frame: CGRect(x: 0, y: self.view.frame.height * 0.2, width: self.view.frame.width, height: self.view.frame.height))
         self.view.addSubview(tf)
-        
+
         var productDataConsolidator = ProductDataConsolidator()
-        
+
         //prices in in nzd
         var products = productDataConsolidator.get()
-        
+
         tf.text = "Prices (NZD):\r"
         for product in products{
             if product != nil{
                 tf.text += "\(product.name): $\(product.price)\r"
             }
         }
-        
+
         //prices in in usd
         products = productDataConsolidator.getInUSD()
-        
+
         tf.text += "\rPrices (USD):\r"
         for product in products{
             if product != nil{
                 tf.text += "\(product.name): $\(product.price)\r"
             }
         }
-        
+
         //prices in in EUR
         products = productDataConsolidator.getInEUR()
-        
+
         tf.text += "\rPrices (EUR):\r"
         for product in products{
             if product != nil{
